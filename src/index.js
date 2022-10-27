37746539be162ea28900de3cd7af9b74123bb7ba
Eliot Jordan <eliotj@princeton.edu>
2022-10-27 12:47:20 -0500

    Extend VectorTileLayer plugin

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import {} from 'leaflet-vector-tile-layer'
+import vectorTileLayer from 'leaflet-vector-tile-layer'
 import {} from 'pmtiles'
+
+
+class PMTilesLayer extends vectorTileLayer { }
+
+L.pmtilesLayer = function (url, options) {
+  return new PMTilesLayer(url, options);
+};
