453764e64b6f227e27bc03ea0ac7804a4fdf1af8
Eliot Jordan <eliotj@princeton.edu>
2022-10-28 12:16:42 -0500

    Initial work on integrating pmtiles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import Pbf from 'pbf'
+import {VectorTile} from '@mapbox/vector-tile'
 import "leaflet.vectorgrid"
-import { PMTiles } from "pmtiles"
+import * as pmtiles from "pmtiles"
 
 L.PMTilesLayer = L.VectorGrid.Protobuf.extend({
 
@@ -98,6 +100,73 @@ L.PMTilesLayer = L.VectorGrid.Protobuf.extend({
 
       return renderer.getContainer();
     },
+
+    _isCurrentTile : function(coords, tileBounds) {
+
+      if (!this._map) {
+        return true;
+      }
+
+      var zoom = this._map._animatingZoom ? this._map._animateToZoom : this._map._zoom;
+      var currentZoom = zoom === coords.z;
+
+      var tileBounds = this._tileCoordsToBounds(coords);
+      var currentBounds = this._map.getBounds().overlaps(tileBounds); 
+
+      return currentZoom && currentBounds;
+
+    },
+
+    _getVectorTilePromise: function(coords, tileBounds) {
+        var data = {
+          s: this._getSubdomain(coords),
+          x: coords.x,
+          y: coords.y,
+          z: coords.z
+        };
+        if (this._map && !this._map.options.crs.infinite) {
+          var invertedY = this._globalTileRange.max.y - coords.y;
+          if (this.options.tms) { // Should this option be available in Leaflet.VectorGrid?
+            data['y'] = invertedY;
+          }
+          data['-y'] = invertedY;
+        }
+
+        if (!this._isCurrentTile(coords, tileBounds)) {
+          return Promise.resolve({layers:[]});
+        }
+
+        // debugger;
+        const p = new pmtiles.PMTiles("https://pul-tile-images.s3.amazonaws.com/pmtiles/parcels.pmtiles")
+
+        return p.getZxy(coords.z, coords.x, coords.y).then(function(arr){
+          if (arr) {
+            var reader = new FileReader();
+            return new Promise(function(resolve){
+              var pbf = new Pbf( arr.data );
+              return resolve(new VectorTile( pbf ));
+            })
+          }
+        }.bind(this)).then(function(json){
+          if (json) {
+            // Normalize feature getters into actual instanced features
+            for (var layerName in json.layers) {
+              var feats = [];
+
+              for (var i=0; i<json.layers[layerName].length; i++) {
+                var feat = json.layers[layerName].feature(i);
+                feat.geometry = feat.loadGeometry();
+                feats.push(feat);
+              }
+
+              json.layers[layerName].features = feats;
+            }
+            return json;
+          } else {
+            return { }
+          }
+        })
+      }
 })
 
 export default L.pmtilesLayer = function (url, options) {
