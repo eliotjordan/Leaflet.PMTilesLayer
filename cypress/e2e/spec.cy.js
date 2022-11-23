04035c7c53d74a0cdf5f2860cc3e8cebd43454c3
Eliot Jordan <eliotj@princeton.edu>
2022-11-23 13:34:29 -0600

    Add Cypress tests and CircleCI

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,26 @@
+describe('Leaflet.PMTilesLayer', () => {
+  describe('polygon tiles', () => {
+    it('renders tiles', () => {
+      cy.visit('cypress/pages/polygon.html')
+
+      cy.get('.leaflet-tile-container')
+        .find('svg')
+        .should('have.length', 16)
+
+      cy.get('.leaflet-tile-container svg').first()
+        .find('g path')
+        .should('have.length', 369)
+    })
+
+    it('renders tiles beyond the max zoom of the pmtiles dataset', () => {
+      cy.visit('cypress/pages/polygon.html')
+
+      cy.wait(4000)
+      cy.get('.leaflet-control-zoom-in').click()
+
+      cy.get('.leaflet-tile-container svg').first()
+        .find('g path')
+        .should('have.length', 369)
+    })
+  })
+})
